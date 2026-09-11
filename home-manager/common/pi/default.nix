82baa09aa6667d95103bc6e9ff90f2a0913998a4
valdo766hi <rivaldosilalahi13@gmail.com>
2026-09-11 16:15:45 +0700

    update pi lazy skill tool

--- home-manager/common/pi/default.nix
+++ home-manager/common/pi/default.nix
@@ -136,7 +136,7 @@ in {
         "npm:@valdo766hi/pi-fast@0.1.2"
         "npm:@valdo766hi/pi-footer@0.1.0"
         "npm:@valdo766hi/pi-yolo@0.1.5"
-        "npm:@valdo766hi/pi-lazy-skill-tool@0.2.0"
+        "npm:@valdo766hi/pi-lazy-skill-tool@0.3.0"
         "npm:@mtrojnar/pi-usage@0.1.6"
         "npm:@ff-labs/pi-fff@0.10.6"
       ];
